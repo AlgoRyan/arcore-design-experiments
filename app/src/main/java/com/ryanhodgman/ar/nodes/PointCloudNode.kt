58da27aace5627bc5b6314a47b8fffb026e76cfb
Ryan Hodgman <ryan.hodgman@outware.com.au>
2018-12-17 16:54:27 +1100

    Includes a confidence indication in the point cloud rendering logic
    * The confidence ARCore provides of a detected feature point's position is now reflected in the colour of the pyramid rendered at that feature point

--- app/src/main/java/com/ryanhodgman/ar/nodes/PointCloudNode.kt
+++ app/src/main/java/com/ryanhodgman/ar/nodes/PointCloudNode.kt
@@ -15,22 +15,50 @@ import kotlin.coroutines.suspendCoroutine
 /**
  * Renders an ARCore point cloud.
  *
+ * Indicates the confidence of each feature point through the colour of that feature point, on a scale from light red
+ * to dark red as the confidence increases.
+ *
  * [Original implementation][https://github.com/claywilkinson/arcore-android-sdk/blob/sceneform-samples/samples/cloud_anchor_java/app/src/main/java/com/google/ar/core/examples/java/cloudanchor/sceneform/PointCloudNode.java]
  * by [Clayton Wilkinson][https://github.com/claywilkinson]
  *
- * @author Clayton Wilkinson, with modifications by Ryan Hodgman
+ * @author Ryan Hodgman, with inspiration from Clayton Wilkinson
  */
 class PointCloudNode : Node() {
 
+    private companion object {
+        private val colourConf0to20 = Color(1f, 1f, 0.8f)
+        private val colourConf20to40 = Color(1f, 0.8f, 0.6f)
+        private val colourConf40to60 = Color(1f, 0.6f, 0.4f)
+        private val colourConf60to80 = Color(1f, 0.4f, 0.2f)
+        private val colourConf80to100 = Color(1f, 0.2f, 0f)
+    }
+
+    private var materialConf0to20: Material? = null
+    private var materialConf20to40: Material? = null
+    private var materialConf40to60: Material? = null
+    private var materialConf60to80: Material? = null
+    private var materialConf80to100: Material? = null
     private var timestamp: Long = 0
-    private var material: Material? = null
 
     suspend fun prepare(context: Context) {
-        material = suspendCoroutine { continuation ->
-            val color = Color().apply {
-                set(android.graphics.Color.RED)
-            }
-            MaterialFactory.makeOpaqueWithColor(context, color)
+        materialConf0to20 = suspendCoroutine { continuation ->
+            MaterialFactory.makeOpaqueWithColor(context, colourConf0to20)
+                    .thenAccept { continuation.resume(it) }
+        }
+        materialConf20to40 = suspendCoroutine { continuation ->
+            MaterialFactory.makeOpaqueWithColor(context, colourConf20to40)
+                    .thenAccept { continuation.resume(it) }
+        }
+        materialConf40to60 = suspendCoroutine { continuation ->
+            MaterialFactory.makeOpaqueWithColor(context, colourConf40to60)
+                    .thenAccept { continuation.resume(it) }
+        }
+        materialConf60to80 = suspendCoroutine { continuation ->
+            MaterialFactory.makeOpaqueWithColor(context, colourConf60to80)
+                    .thenAccept { continuation.resume(it) }
+        }
+        materialConf80to100 = suspendCoroutine { continuation ->
+            MaterialFactory.makeOpaqueWithColor(context, colourConf80to100)
                     .thenAccept { continuation.resume(it) }
         }
     }
@@ -39,35 +67,42 @@ class PointCloudNode : Node() {
      * Update the renderable for the point cloud. This creates a small quad for each feature point.
      */
     fun update(cloud: PointCloud) {
-        // Skip if disabled, if the cloud has not been updated, or if the material hasn't yet been loaded
-        if (!isEnabled || timestamp == cloud.timestamp || material == null) return
+        // Skip if disabled, if the cloud has not been updated, or if the materials haven't yet been loaded
+        if (!isEnabled || timestamp == cloud.timestamp || materialConf0to20 == null || materialConf20to40 == null
+                || materialConf40to60 == null || materialConf60to80 == null || materialConf80to100 == null) return
         timestamp = cloud.timestamp
         if (cloud.numFeatures < 1) {
             // No features in the cloud to be rendered
             renderable = null
             return
         }
-        // Begin collection of the vertices and face indices that represent the structure of the desired Renderable
-        val indices = mutableListOf<Int>()
+        // Create pyramid submeshes that match each feature point
         val vertices = mutableListOf<Vertex>()
+        val submeshes = mutableListOf<Submesh>()
         cloud.features.forEachIndexed { featureIndex, feature ->
-            PyramidMesh(feature).also { mesh ->
-                indices.addAll(mesh.indices.map { meshIndex ->
-                    // Each index matches to a vertex, and there are four vertices per feature
-                    featureIndex * 4 + meshIndex
-                })
-                vertices.addAll(mesh.vertices)
+            val mesh = PyramidMesh(feature)
+            vertices.addAll(mesh.vertices)
+            // Determines the material colour based upon the feature point confidence
+            val material = when (feature.confidence) {
+                in 0.0..0.2 -> materialConf0to20
+                in 0.2..0.4 -> materialConf20to40
+                in 0.4..0.6 -> materialConf40to60
+                in 0.6..0.8 -> materialConf60to80
+                else -> materialConf80to100
             }
+            submeshes.add(Submesh.builder()
+                    .setName("Feature $featureIndex")
+                    .setMaterial(material)
+                    .setTriangleIndices(mesh.indices.map { meshIndex ->
+                        // Each index matches to a vertex, and there are four vertices per feature
+                        featureIndex * 4 + meshIndex
+                    })
+                    .build())
         }
         // Create Renderable for the given point cloud
-        val submesh = Submesh.builder()
-                .setName("pointcloud")
-                .setMaterial(material)
-                .setTriangleIndices(indices)
-                .build()
         val renderableDef = RenderableDefinition.builder()
                 .setVertices(vertices)
-                .setSubmeshes(listOf(submesh))
+                .setSubmeshes(submeshes)
                 .build()
         ModelRenderable.builder()
                 .setSource(renderableDef)
